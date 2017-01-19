8a1a0bd1508386aecd879f3cba60a726d016ac7b
Satyendra-SinghPal <deepak.deepu2020@gmail.com>
2017-01-19 11:50:47 +0530

    PrimeNumber implementation

---
+++ PrimeNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class PrimeNUmber
+{
+	public static boolean isPrime(int num)
+	{
+		int i =2;
+		while(i<n/2)
+		{
+			if(n%i==0)
+				break;
+			i++;
+		}
+		return i>n/2;
+	}
+}
