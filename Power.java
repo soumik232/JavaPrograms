3001ade0a89c1d082c66b67431e1d494b23d39d2
Satyendra-SinghPal <deepak.deepu2020@gmail.com>
2017-01-19 18:05:12 +0530

    Power of 2 upto 31

--- Power.java
+++ Power.java
@@ -2,10 +2,27 @@ class Power
 {
 	public static void main (String[] args)
 	{
-		int a =0;
-		 a = Integer.parseInt(s);
-	//	int b =  Integer.parseInt(args[1]);
-	//	int c = a+b;
-		System.out.println(a);
+		System.out.println("enter a number");
+		int a = Integer.parseInt(System.console().readLine());
+		if(a>0&&a<32)
+		{
+			for(int i = 0;i<a;i++)
+			{
+				int res=isPower(2,i);
+				System.out.println("2^"+i+" ="+res);
+			}
+		}
+		else
+			System.out.println("Range out of bound");
 	}
-} 
+	static int isPower(int b,int p)
+	{
+		int pw=1;
+		while(p>0)
+		{
+			pw=pw*b;
+			p--;
+		}
+		return pw;
+	}
+}
